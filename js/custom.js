130333998db6ffddd2906e8d5f661059333ebe15
sinanmazumder <38401688+sinanmazumder@users.noreply.github.com>
2022-05-12 14:09:09 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+    $("#accordion").accordion({
+       animate: 200,
+       active: false,
+       collapsible: true,
+       heightStyle: "content",
+  });  
+  });
+
+
